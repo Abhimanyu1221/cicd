5b4ca0a03c713da4381f0a1de9c1de72f5f92d76
Abhimanyu1221 <165375245+Abhimanyu1221@users.noreply.github.com>
2026-02-10 14:00:09 +0530

    Change initial counter value expectation to 10

--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,5 @@ test('initial counter value must be zero', () => {
   render(<App />);
 
   const counterElement = screen.getByTestId('counter-display');
-  expect(counterElement.textContent).toBe("0");
+  expect(counterElement.textContent).toBe("10");
 });
