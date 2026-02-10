0848ed24a60468fee6065a5cbe3690390fa487c8
Abhimanyu1221 <165375245+Abhimanyu1221@users.noreply.github.com>
2026-02-10 14:01:01 +0530

    Initialize count state to 0

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css'; 
 
 function App() {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
